e56fb6cf68b45141d07401477003ada837d97c71
Harshada2705 <103305542+Harshada2705@users.noreply.github.com>
2023-12-08 00:08:58 +0530

    Update CategoryRepository.java

--- src/main/java/com/Harsh/springboot/crud/repository/CategoryRepository.java
+++ src/main/java/com/Harsh/springboot/crud/repository/CategoryRepository.java
@@ -1,8 +1,8 @@
-package com.nilesh.springboot.crud.repository;
+package com.harsh.springboot.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nilesh.springboot.crud.model.Category;
+import com.harsh.springboot.crud.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
