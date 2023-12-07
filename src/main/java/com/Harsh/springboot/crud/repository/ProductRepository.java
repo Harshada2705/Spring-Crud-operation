af2013528db8b01ec83a946d3668e06a47845bb0
Harshada2705 <103305542+Harshada2705@users.noreply.github.com>
2023-12-08 00:09:27 +0530

    Update ProductRepository.java

--- src/main/java/com/Harsh/springboot/crud/repository/ProductRepository.java
+++ src/main/java/com/Harsh/springboot/crud/repository/ProductRepository.java
@@ -1,8 +1,8 @@
-package com.nilesh.springboot.crud.repository;
+package com.harsh.springboot.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nilesh.springboot.crud.model.Product;
+import com.harsh.springboot.crud.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
