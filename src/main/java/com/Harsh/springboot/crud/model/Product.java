84aa389f331a2a4d9e84f1944528703536e3467a
Harshada Kumbhar <103305542+Harshada2705@users.noreply.github.com>
2023-12-17 22:23:40 +0530

    Update Product.java

--- src/main/java/com/Harsh/springboot/crud/model/Product.java
+++ src/main/java/com/Harsh/springboot/crud/model/Product.java
@@ -15,58 +15,76 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Product_tbl")
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="product_id")
+   @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
 	private int productid;
-	
-	@Column(name="product_name")
+
+	@Column(name = "product_name")
 	private String productname;
+
+	@Column(name = "product_quantity")
+	private String productQuantity;
+
 	
-	@Column(name="product_quantity")
-	private  String productQuantity;
+	@ManyToOne
+	@JoinColumn(name = "Category_id")
+	private Category category;
+
 
 	public int getProductid() {
 		return productid;
 	}
 
+
 	public void setProductid(int productid) {
 		this.productid = productid;
 	}
+
 
 	public String getProductname() {
 		return productname;
 	}
 
+
 	public void setProductname(String productname) {
 		this.productname = productname;
 	}
+
 
 	public String getProductQuantity() {
 		return productQuantity;
 	}
 
+
 	public void setProductQuantity(String productQuantity) {
 		this.productQuantity = productQuantity;
 	}
-	
-	
-	public Product()
-	{
-		
+
+
+	public Category getCategory() {
+		return category;
 	}
 
-	public Product(int productid, String productname, String productQuantity) {
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Product(int productid, String productname, String productQuantity, Category category) {
 		super();
 		this.productid = productid;
 		this.productname = productname;
 		this.productQuantity = productQuantity;
+		this.category = category;
 	}
-	
-	
-	@ManyToOne
-    @JoinColumn(name = "Category_id")
-    private Category category;
 	
 	
 	
